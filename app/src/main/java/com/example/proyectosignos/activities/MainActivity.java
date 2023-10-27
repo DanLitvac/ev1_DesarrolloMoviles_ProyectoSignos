be0b50d3a89d8658216e3113e469e93e41da6ff0
Danu Litvac <danulitvac02@gmail.com>
2023-10-27 13:13:00 +0200

    Estructura

--- app/src/main/java/com/example/proyectosignos/activities/MainActivity.java
+++ app/src/main/java/com/example/proyectosignos/activities/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.proyectosignos;
+package com.example.proyectosignos.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.proyectosignos.R;
 
 public class MainActivity extends AppCompatActivity {
 
