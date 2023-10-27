534b10e9e7834d466c6f977a0a9e9e3ddaec6503
Danu Litvac <danulitvac02@gmail.com>
2023-10-27 13:41:28 +0200

    Base de datos

---
+++ app/src/main/java/com/example/proyectosignos/model/Palabra.java
@@ -0,0 +1,36 @@
+package com.example.proyectosignos.model;
+import com.example.proyectosignos.app.MyAplication;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+
+public class Palabra extends RealmObject{
+    @PrimaryKey
+    private int id;
+    private String palabra;
+    private Integer imagen;
+    private String categoria;
+
+    public Palabra(String palabra , Integer imagen , String categoria){
+        this.id= MyAplication.palabraID.incrementAndGet();
+        this.palabra=palabra;
+        this.imagen=imagen;
+        this.categoria=categoria;
+    }
+
+    public String getPalabra() {
+        return palabra;
+    }
+
+    public Integer getImagen() {
+        return imagen;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
